754ee0560c308e473d9a08f3545162d0a455714d
Piotr Gorajski <piotr.gorajski@gmail.com>
2020-05-27 15:27:37 +0200

    Handle vineyard pagination and extracting price

--- sommelier/app.py
+++ sommelier/app.py
@@ -2,6 +2,7 @@ import logging
 
 import requests
 from bs4 import BeautifulSoup
+from functional import seq
 
 
 class LidlWine:
@@ -26,22 +27,29 @@ def run():
     url = 'https://winnicalidla.pl/wszystkie-wina/czerwone.html?price=19-30'
     page = requests.get(url)
     soup = BeautifulSoup(page.content, 'html.parser')
-    wine_elements = soup.find_all('li', class_='item col-lg-12')
+
+    # TODO Can I somehow chain html components in single find?
+    pages_count = len(soup.find('div', class_='pages').find_all('li')) - 1
+    logging.info(f'There are {pages_count} pages found')
     lidl_wines = []
-    # TODO Add support for pagination on lidl page
-    print(f'Found {len(wine_elements)} wines')
-    for wine in wine_elements:
-
-        wine_name = wine.find('h2', class_='product-name').find('a')['title']
-        print(wine_name)
-        price_before_comma = wine.find('span', class_='price')
-        if not price_before_comma:
-            pass
-            # print(wine.prettify())
-        # print(price_before_comma)
-
-        # price_after_comma = wine.find('span', class_='sub-price').text
-        # lidl_wines.append(LidlWine(wine_name, f'{price_before_comma},{price_after_comma}'))
-
-    # print(lidl_wines)
+    for page_number in range(1, pages_count + 1):
+        single_page_url = f'https://winnicalidla.pl/wszystkie-wina/czerwone.html?p={page_number}&price=19-30'
+        single_page = requests.get(single_page_url)
+        single_page_soup = BeautifulSoup(single_page.content, 'html.parser')
+        lidl_wines.extend(process_single_vineyard_page(single_page_soup))
+    logging.info(f'Found {len(lidl_wines)} wines')
+    logging.info(lidl_wines)
     logging.info('Application End')
+
+
+def process_single_vineyard_page(soup):
+    wine_elements = soup.find_all('li', class_='item col-lg-12')
+    return seq(wine_elements).map(lambda wine: process_single_wine_element(wine)).to_list()
+
+
+def process_single_wine_element(wine):
+    price_element = wine.find('span', class_='price')
+    if price_element:
+        wine_name = wine.find('h2', class_='product-name').find('a')['title']
+        price = f'{price_element.text[:-2]},{price_element.text[-2:]}'
+        return LidlWine(wine_name, price)
